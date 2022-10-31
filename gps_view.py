abe5d423593f27f54ee0b3869879792797318d2f
wangjinhoon <83506637+wangjinhoon@users.noreply.github.com>
2022-10-31 11:58:24 +0900

    Add files via upload

--- gps_view.py
+++ gps_view.py
@@ -23,8 +23,7 @@ import matplotlib.cm as cm
 # sc = ax.scatter(x,y)
 # plt.xlim(points[0],points[2])
 # plt.ylim(points[1],points[3])
-points = []
-flag = False
+
 class StoppableThread(Thread):
     """Thread class with a stop() method. The thread itself has to check
     regularly for the stopped() condition."""
@@ -44,7 +43,6 @@ def GpsPositionSubCallback(data):
     #print(" Gps 위도 : ", data.latitude, " Gps 경도 : ", data.longitude  )
     gps_latitude =  data.latitude
     gps_longitude = data.longitude
-    flag = False
     # return gps_longitude, gps_latitude
 
 def RtkPositionSubCallback(data):
@@ -55,24 +53,9 @@ def RtkPositionSubCallback(data):
     rtk_longitude = data.longitude
     # return rtk_latitude, rtk_longitude
 
-def scale_to_img(lat_lon, h_w, points):
-        """
-        Conversion from latitude and longitude to the image pixels.
-        It is used for drawing the GPS records on the map image.
-        :param lat_lon: GPS record to draw (lat1, lon1).
-        :param h_w: Size of the map image (w, h).
-        :return: Tuple containing x and y coordinates to draw on map image.
-        """
-        old = (points[2], points[0])
-        new = (0, h_w[1])
-        y = ((lat_lon[0] - old[0]) * (new[1] - new[0]) / (old[1] - old[0])) + new[0]
-        old = (points[1], points[3])
-        new = (0, h_w[0])
-        x = ((lat_lon[1] - old[0]) * (new[1] - new[0]) / (old[1] - old[0])) + new[0]
-        # y must be reversed because the orientation of the image in the matplotlib.
-        # image - (0, 0) in upper left corner; coordinate system - (0, 0) in lower left corner
-        return int(x), h_w[1] - int(y)
-
+def get_drone_yaw_deg_Callback(data):
+    global yaw
+    yaw = data.drone_yaw_deg_
 
 # points: Upper-left, and lower-right GPS points of the map (lat1, lon1, lat2, lon2).
 def listener():
@@ -87,6 +70,7 @@ def listener():
     rospy.init_node('listener', anonymous=True)
     rospy.Subscriber('dji_osdk_ros/gps_position', NavSatFix, GpsPositionSubCallback)
     rospy.Subscriber('dji_osdk_ros/rtk_position', NavSatFix, RtkPositionSubCallback)
+    # rospy.Subscriber('dji_osdk_ros/drone_yaw_deg_', yaw , get_drone_yaw_deg_Callback)
     list_latitude = []
     list_longitude = []
     img_points_x = []
@@ -101,24 +85,15 @@ def listener():
     t = StoppableThread(target=function)
     t.start()
     while True:
-        if rtk_latitude == 0 and rtk_longitude == 0:
-            offset_latitude = gps_latitude
-            offset_longitude = gps_longitude
-            offset_signal = "gps"
-        else:
-            offset_latitude = rtk_latitude
-            offset_longitude = rtk_longitude
-            offset_signal = "rtk"
+        if max_lat < gps_latitude:
+            max_lat = gps_latitude + 0.00005
+        if min_lat > gps_latitude:
+            min_lat = gps_latitude - 0.00005
         
-        if max_lat < offset_latitude:
-            max_lat = offset_latitude + 0.00005
-        if min_lat > offset_latitude:
-            min_lat = offset_latitude - 0.00005
-        
-        if max_lon < offset_longitude:
-            max_lon = offset_longitude + 0.00005
-        if min_lon > offset_longitude:
-            min_lon = offset_longitude - 0.00005
+        if max_lon < gps_longitude:
+            max_lon = gps_longitude + 0.00005
+        if min_lon > gps_longitude:
+            min_lon = gps_longitude - 0.00005
         
         # if before_offset_latitude == offset_latitude and before_offset_longitude == offset_longitude:
         #     count += 1
@@ -127,11 +102,16 @@ def listener():
         # else:
         #     count = 0
         
-        before_offset_latitude = offset_latitude
-        before_offset_longitude = offset_longitude
+        # before_gps_latitude = gps_latitude
+        # before_gps_longitude = gps_longitude
+
+        # before_rtk_latitude = rtk_latitude
+        # before_rtk_longitude = rtk_longitude
         # print("out",offset_latitude, offset_longitude)
-        d = [offset_latitude, offset_longitude , offset_signal]
+        gps_d = [gps_latitude, gps_longitude]
+        rtk_d = [rtk_latitude, rtk_longitude]
         points = [min_lat, min_lon, max_lat, max_lon]
+        d = [gps_d, rtk_d]
         # print("최소 위도 : ",min_lat, "최소 경도 : ",min_lon, "최대 위도 : ",max_lat, "최대 경도 : ",max_lon)
         # print(d)
         # result_image = Image.open("map.png", 'r')
@@ -151,22 +131,27 @@ def listener():
 
 def function():
     time.sleep(0.01)
-    global x1, y1
+    global x1, y1 , x2, y2
     global d
     global points
-    global intensity
-    d = [0, 0]
+    global intensity1, intensity2, intensity3 ,intensity4
+
+    gps_d = [0, 0]
+    rtk_d = [0, 0]
     fig, ax = plt.subplots()
-    x,y = [], []
-    intensity = []
+    x1,y1 = [], []
+    x2,y2 = [], []
+    intensity1 = []
+    intensity2 = []
+    intensity3 = []
+    intensity4 = []
     iterations = 100
     t_vals = np.linspace(0,1, iterations)
-    colors = [[0,0,1,0],[0,0,1,0.5],[0,0.2,0.4,1]]
-    if d[2] == "rtk":
-        cmap = cm.get_cmap('Blues')
-    else:
-        cmap = cm.get_cmap('Reds')
-    sc = ax.scatter(x,y,c=[],cmap=cmap, vmin=0,vmax=1)# c=np.tile(z, (100, 1))
+    cmaps=[cm.get_cmap('Reds'),cm.get_cmap('Blues'),cm.get_cmap('Oranges'),cm.get_cmap('Greens')] #declaring colormaps
+    sc1 = ax.scatter(x1,y1,c=[],cmap=cmaps[0],vmin=0,vmax=1) #initializing the 3 scatter plots
+    sc2 = ax.scatter(x2,y2,c=[],cmap=cmaps[1],vmin=0,vmax=1) #initializing the 3 scatter plots
+    sc3 = ax.scatter(d[0][1],d[0][0],c=[],cmap=cmaps[2],vmin=0,vmax=1) #initializing the 3 scatter plots
+    sc4 = ax.scatter(d[1][1],d[1][0],c=[],cmap=cmaps[3],vmin=0,vmax=1) #initializing the 3 scatter plots
     plt.title('grid length : 1m')
     plt.xlabel('longitude')
     plt.ylabel('latitude')
@@ -178,21 +163,34 @@ def function():
     plt.yticks(np.arange(points[0], points[2], 0.00001),color='w'    ) # ,color='w'    
     plt.grid(True)                       
     def animate(i):
-        global intensity
+        global intensity1, intensity2,intensity3 ,intensity4
         # x.append(x1)
         # y.append(y1)
         # print("thread : ", d)
-        x.append(d[1])
-        y.append(d[0])
+        x1.append(d[0][1])
+        y1.append(d[0][0])
+        x2.append(d[1][1])
+        y2.append(d[1][0])
         ax.set_xlim(points[1],points[3])#축
         ax.set_ylim(points[0],points[2])
         ax.set_xticks(np.arange(points[1], points[3], 0.00001)) #1m 좌표
         ax.set_yticks(np.arange(points[0], points[2], 0.00001))    
-        sc.set_offsets(np.c_[x,y])
-        
-        #calculate new color values
-        intensity = np.concatenate((np.array(intensity)*0.998, np.ones(1)))
-        sc.set_array(intensity)
+        sc1.set_offsets(np.c_[x1,y1])
+        sc2.set_offsets(np.c_[x2,y2])
+        sc3.set_offsets(np.c_[d[0][1],d[0][0]])
+        sc4.set_offsets(np.c_[d[1][1],d[1][0]])
+    #calculate new color values
+        intensity1 = np.concatenate((np.array(intensity1)*0.995, np.ones(1)))
+        intensity2 = np.concatenate((np.array(intensity2)*0.995, np.ones(1)))
+        # intensity3 = np.concatenate((np.array(intensity3)*0.5, np.ones(1)))
+        # intensity4 = np.concatenate((np.array(intensity4)*0.5, np.ones(1)))
+        sc1.set_array(intensity1)
+        sc2.set_array(intensity2)
+        sc3.set_array([d[0][1],d[0][0]])
+        sc4.set_array([d[1][1],d[1][0]])
+
+
+
     
     ani = matplotlib.animation.FuncAnimation(fig, animate, 
                     frames=t_vals, interval=50) 
